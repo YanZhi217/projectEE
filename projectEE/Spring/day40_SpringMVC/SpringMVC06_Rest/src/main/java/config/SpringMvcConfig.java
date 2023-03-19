055322785952421a828aa5d74c657834cf655ea2
YanZhi <2876048156@qq.com>
2023-03-19 22:06:52 +0800

    day40.2

---
+++ projectEE/Spring/day40_SpringMVC/SpringMVC06_Rest/src/main/java/config/SpringMvcConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.Filter;
+import javax.servlet.annotation.WebFilter;
+
+@Configuration
+@ComponentScan("controller")
+@EnableWebMvc
+public class SpringMvcConfig {
+}
